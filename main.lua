cac78d336d3da01187282b23589fb7c4cbcda8c1
WriteCoin <sovtsardom@gmail.com>
2021-08-23 23:25:25 +0300

    код сужен в один файл, readme обновлен

--- main.lua
+++ main.lua
@@ -1,5 +1,28 @@
-local initGlobals = InitGlobals
-function InitGlobals()
-    initGlobals()
-    require 'src.index'
-end
+compiletime(function()
+    local mapName = 'test1.w3x' -- имя карты
+
+    local path = ceres.layout.mapsDirectory .. mapName
+    print('path: ', path)
+    local map, err = ceres.openMap(path)
+    print('map, err: ', map, err)
+
+    local addNewObjects = function(keyObjects, targetMap, sourceMap)
+        for id in pairs(sourceMap.objects[keyObjects].all) do
+            if (not targetMap.objects[keyObjects]:getObject(id)) then
+                targetMap.objects[keyObjects]:setObject(id, sourceMap.objects[keyObjects]:getObject(id))
+            end
+        end
+    end
+
+    local function addNewAllObjects(targetMap, sourceMap)
+        addNewObjects('unit', targetMap, sourceMap)
+        addNewObjects('item', targetMap, sourceMap)
+        addNewObjects('destructable', targetMap, sourceMap)
+        addNewObjects('doodad', targetMap, sourceMap)
+        addNewObjects('ability', targetMap, sourceMap)
+        addNewObjects('buff', targetMap, sourceMap)
+        addNewObjects('upgrade', targetMap, sourceMap)
+    end
+
+    addNewAllObjects(currentMap, map)
+end)
